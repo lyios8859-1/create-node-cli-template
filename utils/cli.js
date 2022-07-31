96381a817a4e33b3ec44fcc6a8a585779cd7ace0
七月十五 <194962218@qq.com>
2022-07-31 17:24:59 +0800

    :sparkles: refactor

--- utils/cli.js
+++ utils/cli.js
@@ -1,75 +1,93 @@
 const meow = require("meow");
-const { green, yellow, cyan, dim } = require("chalk");
+const meowHelper = require("./cli-meow-help");
 
-const helpText = `
-  Usage
-    ${dim("$")} ${green("npx yun-node")} ${yellow("[options]")} ${cyan(
-  "<commands>"
-)}
-  
-  Options
-    ${yellow("social")}        Print the social info ${dim("(DEFATULT: true)")}
-    ${yellow("--no-social")}   Don't print the social info
-    ${yellow("bio")}           Print the bio info ${dim("(DEFATULT: true)")}
-    ${yellow("--no-bio")}      Don't print the bio info
-    ${yellow("ad")}            Print the Ad info ${dim("(DEFATULT: true)")}
-    ${yellow("--no-ad")}       Don't print the Ad info
-    ${yellow("clear")}         Clear the console ${dim("(DEFATULT: true)")}
-    ${yellow("--no-clear")}    Don't clear the console
-    ${yellow("-s")}, ${yellow("--stats")}   Print stats info
-    ${yellow("-p")}, ${yellow("--posts")}   Print posts info
-    ${yellow("-d")}, ${yellow("--debug")}   Print debug info
-    ${yellow("-m")}, ${yellow("--minimal")} Print minimal info
-    ${yellow("-v")}, ${yellow("--version")} Print CLI version
-
-  Commands
-    ${cyan("help")}          Print CLI help info
-
-  Examples
-    ${dim("$")} ${green("npx yun-node")} ${yellow("--no-social")}
-    ${dim("$")} ${green("npx yun-node")} ${cyan("help")}
-`;
-const options = {
-  inferType: true, // 保证输入的数字或者数字字符串是数字: 5, "5" => 5
-  hardRejection: false, // 是否使用自己的错误打印信息
-  flags: {
-    minimal: {
-      type: "boolean",
-      alias: "m",
-    },
-    clear: {
-      type: "boolean",
-      default: true,
-    },
-    bio: {
-      type: "boolean",
-      default: true,
-    },
-    social: {
-      type: "boolean",
-      default: true,
-    },
-    ad: {
-      type: "boolean",
-      default: true,
-    },
-    posts: {
-      type: "boolean",
-      alias: "p",
-    },
-    stats: {
-      type: "boolean",
-      alias: "s",
-    },
-    debug: {
-      type: "boolean",
-      alias: "d",
-    },
-    version: {
-      type: "boolean",
-      alias: "v",
-    },
+const flags = {
+  ad: {
+    type: "boolean",
+    default: true,
+    desc: "Print Ad. info",
   },
+  "no-ad": {
+    type: "boolean",
+    default: false,
+    desc: "Don't Print Ad. info",
+  },
+  bio: {
+    type: "boolean",
+    default: true,
+    desc: "Print bio info",
+  },
+  "no-bio": {
+    type: "boolean",
+    default: false,
+    desc: "Don't print bio info",
+  },
+  clear: {
+    type: "boolean",
+    default: true,
+    desc: "Clear the console",
+  },
+  "no-clear": {
+    type: "boolean",
+    default: false,
+    desc: "Don't clear the console",
+  },
+  social: {
+    type: "boolean",
+    default: true,
+    desc: "Print social info",
+  },
+  "no-social": {
+    type: "boolean",
+    default: false,
+    desc: "Don't print social info",
+  },
+  minimal: {
+    type: "boolean",
+    alias: "m",
+    desc: "Print minimal info",
+  },
+  posts: {
+    type: "boolean",
+    alias: "p",
+    desc: "Print posts info",
+  },
+  stats: {
+    type: "boolean",
+    alias: "s",
+    desc: "Print stats info",
+  },
+  debug: {
+    type: "boolean",
+    alias: "d",
+    desc: "Print debug info",
+  },
+  version: {
+    type: "boolean",
+    alias: "v",
+    desc: "Print CLI version",
+  },
+};
+
+const commands = {
+  help: {
+    desc: "Print CLI help info",
+  },
+};
+
+const helpText = meowHelper({
+  name: "npx @timly/yun",
+  flags,
+  commands,
+  defaults: true, // 显示默认值
+  desc: "CLI Options Docs",
+});
+
+const options = {
+  inferType: true,
+  hardRejection: false,
+  description: false,
+  flags,
 };
 
 module.exports = meow(helpText, options);
